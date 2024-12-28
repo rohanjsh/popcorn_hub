0cd9a3e9c43a7fa489c3e5847a66f68ae8357761
Rohan <rohan.joshi@goodcreator.co>
2024-12-28 10:46:44 +0530

    chore: initialise project

---
+++ android/app/src/main/kotlin/dev/rohanjsh/popcorn/hub/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.rohanjsh.popcorn_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:popcorn_hub/app/app.dart';
+import 'package:popcorn_hub/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
